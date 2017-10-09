363c4e3994b15a0010eaca7022d82d1f8bf1e85c
Niklas Weiler <niklas.weiler@uni-konstanz.de>
2017-10-09 11:10:08 +0200

    Added Datamodel and dataloader

---
+++ Assets/Scripts/Model/NumericDatamodel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class NumericDatamodel : MonoBehaviour {
+
+    public string nameOfDatafile;
+    public bool hasheader;
+    public string delimiter;
+
+    public float[][] myData;
+    public string[] myHeaders;
+
+    // Use this for initialization
+    void Start () {
+        if (nameOfDatafile.EndsWith(".csv"))
+        {
+            myData = DatasetImporter.readNumericCSV(nameOfDatafile, hasheader, delimiter);
+            myHeaders = DatasetImporter.getHeader(nameOfDatafile, delimiter);
+            return;
+        }
+
+        throw new FileLoadException("Unsupported data format of file '" + nameOfDatafile + "'. Can not load data into datamodel.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
