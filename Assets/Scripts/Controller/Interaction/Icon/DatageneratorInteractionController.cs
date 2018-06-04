a7bddd6bae5e88823f5902f1f78cc59f42b0bca3
Matthias Kraus <matthias.kraus@uni-konstanz.de>
2018-06-04 12:26:13 +0200

    Scaffolding for datagenerator operator

---
+++ Assets/Scripts/Controller/Interaction/Icon/DatageneratorInteractionController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Controller.Interaction.Icon
+{
+    public class DatageneratorInteractionController : GenericIconInteractionController
+    {
+
+        protected override void OnLeftClickOnTargetEventAction()
+        {
+            Debug.Log("Datagenerator Icon OnLeftClickEvent");
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Operators/DatageneratorOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Assets.Scripts.Model;
+using UnityEngine;
+
+public class DatageneratorOperator : GenericOperator
+{
+    public override bool Process()
+    {
+
+        // add here data generation
+        // Analog to Dataloader: 
+        // var dataModel = ReadCsv().MergeDatamodels(_rawInputData);
+        // SetOutputData(dataModel);
+
+        return true;
+    }
+
+    public override bool ValidateIfOperatorPossibleForParents(GenericOperator parent)
+    {
+        return true;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+}
