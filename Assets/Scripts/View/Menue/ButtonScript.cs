1fafad9da4068ead6526843358a7bb4daeac17fe
Daniel Schweitzer <daniel.schweitzer@uni-konstanz.de>
2018-08-28 16:03:49 +0200

    button animation

--- Assets/Scripts/View/Menue/ButtonScript.cs
+++ Assets/Scripts/View/Menue/ButtonScript.cs
@@ -5,32 +5,18 @@ using UnityEngine.UI;
 
 public class ButtonScript : GenericMenueComponent {
 
-//	public Animator myAnimator;
-	public Material PressedMaterial;
-
-	private Transform Button;
-
-	private void Start()
-	{
-		Button = gameObject.transform.GetChild(1);
-	}
+	public Animator myAnimator;
 
 	//what happens when the button is triggered
 	public void Toggle()
 	{
-		Debug.Log("toggle");
-		//at first toggle the color of the button
-		var oldMaterial = Button.GetComponent<Renderer>().sharedMaterial;
-		Button.GetComponent<Renderer>().material = PressedMaterial;
-		
-		//then toggle the animation
-		// TODO: animation
+		myAnimator.SetTrigger("press");
+
 		List<IMenueComponentListener> listeners = getListeners();
 		foreach(IMenueComponentListener listener in listeners)
 		{
 			listener.menueChanged(this);
-		}
-		
+		}	
 	}
 
 	//probably is useless?
